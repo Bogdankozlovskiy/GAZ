bdf9220b09161ea83af99d5690f39ea5f6e10400
berloger <ogir@bk.ru>
2020-08-01 09:14:54 +0300

    add create user function

--- administration/forms.py
+++ administration/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
 User = get_user_model()
 
-class UserRegForm(forms.ModelForm):
+# class UserRegForm(forms.ModelForm):
+#     class Meta:
+#         model = User
+#         fields = '__all__'
+
+class UserRegForm(UserCreationForm):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ("username",)
--- administration/views.py
+++ administration/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
 from . import forms
 from django.contrib.auth import get_user_model
+# from django.contrib.auth.forms import UserCreationForm
 
 User = get_user_model()
 
 # Create your views here.
 def index(request):
     # users_list = CustomUser.objects.all()
-    users_list = User.objects.all()
+    users_list = User.objects.exclude(is_staff=True)
     context = {
         "users_list": users_list,
     }
@@ -19,10 +20,13 @@ def user_reg(request):
 
     if request.method == 'POST':
         form = forms.UserRegForm(request.POST)
+        # form = UserCreationForm(request.POST)
         if form.is_valid():
-            pass
+            form.save()
+            return redirect('/administration/')
 
     form_class = forms.UserRegForm
+    # form_class = UserCreationForm
     context = {
         "form": form_class,
     }
